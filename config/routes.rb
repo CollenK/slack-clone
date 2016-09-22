ac1452a6637e6418ba5533d5b8128408b59d77ac
CollenK <collen.kriel@gmail.com>
2016-09-22 17:04:53 +0700

    Set up Join/Leave for Chatrooms; baisc layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :chatrooms
+  resources :chatrooms do
+    resource :chatroom_users
+  end
 
   root to: "chatrooms#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
