784f288d076b2514db3fa71a2a9848d7f34a29f3
CollenK <collen.kriel@gmail.com>
2016-10-20 10:22:44 +0700

    Add direct messages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :messages
   end
 
+  resources :direct_messages
+
   root to: "chatrooms#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015022720) do
+ActiveRecord::Schema.define(version: 20161020022534) do
 
   create_table "chatroom_users", force: :cascade do |t|
     t.integer  "chatroom_id"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20161015022720) do
 
   create_table "chatrooms", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.boolean  "direct_message", default: false
   end
 
   create_table "messages", force: :cascade do |t|
